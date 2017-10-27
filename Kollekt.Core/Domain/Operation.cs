826d69c82616c8865f9ddfe874173c4dc3fee3c9
pospieszja <j.pospieszynski@outlook.com>
2017-10-27 15:46:23 +0200

    Added inheritance

--- Kollekt.Core/Domain/Operation.cs
+++ Kollekt.Core/Domain/Operation.cs
@@ -2,7 +2,7 @@ using System;
 
 namespace Kollekt.Core.Domain
 {
-    public class Operation
+    public class Operation: Entity
     {
         public string Name { get; set; }
         public Guid WorkplaceId { get; set; }
